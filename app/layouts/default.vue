82b2c3dc52142dbd030cadbb07ddb933a95ccd5d
loumouli <louis.moulin@outlook.fr>
2024-04-16 19:50:04 +0200

    add vercel analytics

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import {SpeedInsights} from '@vercel/speed-insights/nuxt';
 import {WalletMultiButton} from 'solana-wallets-vue';
 </script>
 
 <template>
+  <SpeedInsights />
   <ClientOnly>
     <WalletMultiButton />
   </ClientOnly>
